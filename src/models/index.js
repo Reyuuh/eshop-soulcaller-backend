27165acdc7109474acf495bafb333f37969c2d57
Inti <intirey4@gmail.com>
2025-12-22 15:22:03 +0100

    relocated app.js with index.js

---
+++ src/models/index.js
@@ -0,0 +1,5 @@
+const sequelize = require("./sequelize.js");
+const User = require("./user.js");
+const Product = require("./product.js");
+const Order = require("./order.js");
+const OrderItem = require("./orderItem.js");

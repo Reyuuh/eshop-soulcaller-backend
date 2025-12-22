c503e20e51d128bd173b39632fb3cde8f963c915
Inti <intirey4@gmail.com>
2025-12-22 15:33:34 +0100

    added all the connection with models

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,25 @@
 const sequelize = require("./sequelize.js");
-const User = require("./user.js");
-const Product = require("./product.js");
-const Order = require("./order.js");
-const OrderItem = require("./orderItem.js");
+const User = require("./UserModel.js");
+const Product = require("./ProductModel.js");
+const Order = require("./OrderModel.js");
+const OrderItem = require("./OrderItemModel.js");
+const Category = require("./CategoryModel.js");
+
+// Define associations
+User.hasMany(Order, { foreignKey: "user_id" });
+Order.belongsTo(User, { foreignKey: "user_id" });
+
+Order.hasMany(OrderItem, { foreignKey: "order_id" });
+OrderItem.belongsTo(Order, { foreignKey: "order_id" });
+
+Product.belongsTo(Category, { foreignKey: "category_id" });
+Category.hasMany(Product, { foreignKey: "category_id" });
+
+module.exports = {
+  sequelize,
+  User,
+  Product,
+  Order,
+  OrderItem,
+  Category,
+};

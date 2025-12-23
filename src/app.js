50f7fc8a17b2f31c8e570a125540e3b2c6698b0b
Inti <intirey4@gmail.com>
2025-12-23 13:44:27 +0100

    added middleware,utils and categories controller

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import sequelize from "./models/sequelize.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 app.use(cors());
@@ -23,3 +24,5 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server listening on port ${PORT}`);
 });
+
+app.use(errorHandler);
